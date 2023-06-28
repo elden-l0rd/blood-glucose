127d3b68fa70d53a618cd48367b15732ade10ee2
elden-l0rd <cfhuangx9@gmail.com>
2023-06-28 17:23:10 +0800

    Add graph filtering functionality

--- max_heart_reader/lib/Client/src/LandingPage/home.dart
+++ max_heart_reader/lib/Client/src/LandingPage/home.dart
@@ -15,20 +15,46 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  String dropdownValue = timeSelectionList.first;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
         children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 87, 86, 86),
+            ),
+            child: DropdownButton<String>(
+              value: dropdownValue,
+              icon: const Icon(Icons.arrow_downward),
+              elevation: 16,
+              style: const TextStyle(color: Colors.white),
+              dropdownColor: const Color.fromARGB(255, 87, 86, 86),
+              onChanged: (String? value) {
+                setState(() {
+                  dropdownValue = value!;
+                });
+              },
+              items:
+                  timeSelectionList.map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
           Expanded(
             // View graph
             child: Container(
               height: 700,
               padding: EdgeInsets.all(8.0),
               child: FutureBuilder<List<graphData>>(
-                // future: DatabaseHelper.instance.getGraphDataList(filterMonth: '2023-06-01'),
-                future: DatabaseHelper.instance.getGraphDataList(),
+                future: _fetchGraphData(),
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
                     return buildChart(snapshot.data!);
@@ -47,9 +73,99 @@ class HomePageState extends State<HomePage> {
               ),
             ),
           ),
+          ElevatedButton(
+          onPressed: () {
+            nextPeriod();
+          },
+          child: Text('Next Period'),
+        ),
+          ElevatedButton(
+            onPressed: () {
+              _updateGraphData();
+            },
+            child: Text('Update Graph Data'),
+          ),
         ],
       ),
     );
+  }
+
+  Future<List<graphData>> _fetchGraphData({String? filter}) async {
+    if (dropdownValue == 'All') {
+      return await DatabaseHelper.instance.getGraphDataList();
+    } else if (dropdownValue == 'Day') {
+      if (filter != null) {
+        return await DatabaseHelper.instance.getGraphDataList(filterDay: filter);
+      }
+      DateTime now = DateTime.now();
+      String filterDay = DateTime(now.year, now.month, now.day).toString();
+      return await DatabaseHelper.instance.getGraphDataList(filterDay: filterDay);
+    } else if (dropdownValue == 'Week') {
+      if (filter != null) {
+        return await DatabaseHelper.instance.getGraphDataList(filterWeek: filter);
+      }
+      DateTime now = DateTime.now();
+      DateTime startOfWeek = now.subtract(Duration(days: now.weekday - 1));
+      String filterWeek = startOfWeek.toString();
+      return await DatabaseHelper.instance.getGraphDataList(filterWeek: filterWeek);
+    } else if (dropdownValue == 'Month') {
+      if (filter != null) {
+        return await DatabaseHelper.instance.getGraphDataList(filterMonth: filter);
+      }
+      DateTime now = DateTime.now();
+      String filterMonth = DateTime(now.year, now.month).toString();
+      return await DatabaseHelper.instance.getGraphDataList(filterMonth: filterMonth);
+    } else if (dropdownValue == 'Year') {
+      if (filter != null) {
+        return await DatabaseHelper.instance.getGraphDataList(filterYear: filter);
+      }
+      DateTime now = DateTime.now();
+      String filterYear = DateTime(now.year).toString();
+      return await DatabaseHelper.instance.getGraphDataList(filterYear: filterYear);
+    }
+    return [];
+  }
+
+
+  void _updateGraphData() {
+    setState(() {});
+  }
+
+  void nextPeriod() {
+    if (dropdownValue == 'Day') {
+      // Increment the current date by one day
+      DateTime currentDate = DateTime.now().add(Duration(days: 1));
+      String filterDay = DateTime(currentDate.year, currentDate.month, currentDate.day).toString();
+      setState(() {
+        dropdownValue = 'Day';
+        _fetchGraphData(filter: filterDay); // Fetch data for the next day
+      });
+    } else if (dropdownValue == 'Week') {
+      // Increment the current date by one week
+      DateTime currentDate = DateTime.now().add(Duration(days: 7));
+      DateTime startOfWeek = currentDate.subtract(Duration(days: currentDate.weekday - 1));
+      String filterWeek = startOfWeek.toString();
+      setState(() {
+        dropdownValue = 'Week';
+        _fetchGraphData(filter: filterWeek); // Fetch data for the next week
+      });
+    } else if (dropdownValue == 'Month') {
+      // Increment the current date by one month
+      DateTime currentDate = DateTime.now().add(Duration(days: 30));
+      String filterMonth = DateTime(currentDate.year, currentDate.month).toString();
+      setState(() {
+        dropdownValue = 'Month';
+        _fetchGraphData(filter: filterMonth); // Fetch data for the next month
+      });
+    } else if (dropdownValue == 'Year') {
+      // Increment the current date by one year
+      DateTime currentDate = DateTime.now().add(Duration(days: 365));
+      String filterYear = DateTime(currentDate.year).toString();
+      setState(() {
+        dropdownValue = 'Year';
+        _fetchGraphData(filter: filterYear); // Fetch data for the next year
+      });
+    }
   }
 
   Widget buildChart(List<graphData> data) {
@@ -57,63 +173,236 @@ class HomePageState extends State<HomePage> {
 
     // !!!!!!!  for Trilogy marketing video  !!!!!!!
     // !!!!!!!  COMMENT OUT for actual code  !!!!!!!
-    void generateRandomData() {
-      DateTime startDate = DateTime(2023, 6, 8);
-      Random random = Random();
-      List<Duration> durations = [];
+    // void generateRandomData() {
+    //   DateTime startDate = DateTime(2023, 6, 8);
+    //   Random random = Random();
+    //   List<Duration> durations = [];
+    //
+    //   for (int i = 0; i < 3; i++) {
+    //     Duration randomDuration = Duration(days: random.nextInt(30));
+    //     durations.add(randomDuration);
+    //   }
+    //   durations.sort();
+    //   DateTime currentDate = startDate;
+    //
+    //   for (int i = 0; i < durations.length; i++) {
+    //     currentDate = currentDate.add(durations[i]);
 
-      for (int i = 0; i < 8; i++) {
-        Duration randomDuration = Duration(days: random.nextInt(30));
-        durations.add(randomDuration);
-      }
-      durations.sort();
-      DateTime currentDate = startDate;
-
-      for (int i = 0; i < 8; i++) {
-        currentDate = currentDate.add(durations[i]);
-
-        // Ensure the current date does not exceed the end date
-        if (currentDate.isAfter(DateTime(2025, 6, 8))) {
-          break;
-        }
-
-        String timestamp = currentDate.toString();
-        String formattedTimestamp = timestamp.split('.')[0];
-
-        graphData data = graphData(
-          timestamp: formattedTimestamp,
-          batteryText: '38%',
-          heartRateText: '77 bpm',
-          spo2Text: '98%',
-          glucose_mmolL: 2.0 + Random().nextDouble() * 11,
-          glucose_mgDL: .01,
-          cholesterolText: '2.01 mg/dL',
-          UA_menText: 'Normal: 0.32 mmol/L',
-          UA_womenText: 'Normal: 0.26 mmol/L',
-        );
-
-        rowData.add(data);
-      }
-      print("Data added successfully!");
-    }
-
-    generateRandomData();
-    // rowData = [
-    //   graphData(
-    //     timestamp: '2023-06-08 12:00:00',
-    //     batteryText: '38%',
-    //     heartRateText: '77 bpm',
-    //     spo2Text: '98%',
-    //     glucose_mmolL: 2.0 + Random().nextDouble() * 11,
-    //     glucose_mgDL: .01,
-    //     cholesterolText: '2.01 mg/dL',
-    //     UA_menText: 'Normal: 0.32 mmol/L',
-    //     UA_womenText: 'Normal: 0.26 mmol/L',
-    //   ),
-    //   .
-    //   .
-    //   .
-    // ];
+    //     // Ensure the current date does not exceed the end date
+    //     if (currentDate.isAfter(DateTime(2025, 6, 8))) {
+    //       break;
+    //     }
+    //
+    //     String timestamp = currentDate.toString();
+    //     String formattedTimestamp = timestamp.split('.')[0];
+    //
+    //     graphData data = graphData(
+    //       timestamp: formattedTimestamp,
+    //       batteryText: '38%',
+    //       heartRateText: '77 bpm',
+    //       spo2Text: '98%',
+    //       glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+    //       glucose_mgDL: .01,
+    //       cholesterolText: '2.01 mg/dL',
+    //       UA_menText: 'Normal: 0.32 mmol/L',
+    //       UA_womenText: 'Normal: 0.26 mmol/L',
+    //     );
+    //
+    //     rowData.add(data);
+    //   }
+    //   print("Data added successfully!");
+    // }
+    //
+    // generateRandomData();
+    rowData = [
+      // graphData(
+      //   timestamp: '2023-06-08 12:00:00',
+      //   batteryText: '38%',
+      //   heartRateText: '77 bpm',
+      //   spo2Text: '98%',
+      //   glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+      //   glucose_mgDL: .01,
+      //   cholesterolText: '2.01 mg/dL',
+      //   UA_menText: 'Normal: 0.32 mmol/L',
+      //   UA_womenText: 'Normal: 0.26 mmol/L',
+      // ),
+      graphData(
+        timestamp: '2023-06-26 12:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2023-06-26 18:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2023-06-28 18:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2023-06-30 18:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2023-06-30 18:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2023-07-01 12:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2023-07-01 19:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2023-07-22 19:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2023-07-23 19:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2023-07-25 19:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2024-07-25 19:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2024-07-25 19:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2024-07-27 19:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2024-08-13 19:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2025-02-28 19:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+      graphData(
+        timestamp: '2025-03-13 19:00:00',
+        batteryText: '38%',
+        heartRateText: '77 bpm',
+        spo2Text: '98%',
+        glucose_mmolL: 2.0 + Random().nextDouble() * 11,
+        glucose_mgDL: .01,
+        cholesterolText: '2.01 mg/dL',
+        UA_menText: 'Normal: 0.32 mmol/L',
+        UA_womenText: 'Normal: 0.26 mmol/L',
+      ),
+    ];
 
     // insert data into db
     for (graphData row in rowData) {
@@ -133,31 +422,6 @@ class HomePageState extends State<HomePage> {
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 16.0),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 87, 86, 86),
-          ),
-          child: DropdownButton<String>(
-            value: dropdownValue,
-            icon: const Icon(Icons.arrow_downward),
-            elevation: 16,
-            style: const TextStyle(color: Colors.white),
-            dropdownColor: const Color.fromARGB(255, 87, 86, 86),
-            onChanged: (String? value) {
-              setState(() {
-                dropdownValue = value!;
-              });
-            },
-            items:
-                timeSelectionList.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-          ),
-        ),
         Expanded(
           child: SfCartesianChart(
             title: ChartTitle(
